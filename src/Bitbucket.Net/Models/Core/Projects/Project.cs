7d7f985af100ca734e08ae2dab2c983b29351bc7
barapiiro <bar@apiiro.com>
2025-03-26 17:36:26 +0200

    Move `Public` prop to the base `ProjectRef`
    in order to consume the project.public from `Repository` holds a `ProjectRef` and not `Project` (though it seems to actually return something more similar to `Project` from the raw rest api).
    
    I could change the Repository to hold Project instead, but it's being used all over, I wanted to only add this specific field and not the entire model.
    
    but I think for the long run, we should ditch this package completely and just implement our own rest-api client based on the bitbucketserver docs.

--- src/Bitbucket.Net/Models/Core/Projects/Project.cs
+++ src/Bitbucket.Net/Models/Core/Projects/Project.cs
@@ -3,7 +3,6 @@
     public class Project : ProjectDefinition
     {
         public int Id { get; set; }
-        public bool Public { get; set; }
         public string Type { get; set; }
         public Links Links { get; set; }
 
